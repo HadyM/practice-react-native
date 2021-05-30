61c220765e0391065995618f92d7affd73e807f1
Hady Mohamed <hadymohamed@pursuit.org>
2021-05-29 21:51:42 -0400

    practice

--- App.js
+++ App.js
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
   },
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "red",
     padding: 20,
     borderRadius: 5,
   },
